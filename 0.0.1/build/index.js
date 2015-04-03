c15525dd1fc7d19b67b9f4e6e5e2b5960c1b72c5
kissyteam <tiehang.lth@taobao.com>
2015-04-03 13:39:14 +0800

    sync daily version 0.0.1

--- 0.0.1/build/index.js
+++ 0.0.1/build/index.js
@@ -1,4 +1,4 @@
-//! Copyright 2015, kissy-gallery-swf@0.0.1 MIT Licensed, build time: Thu, 05 Feb 2015 09:48:50 GMT 
+//! Copyright 2015, kissy-gallery-swf@0.0.1 MIT Licensed, build time: Fri, 03 Apr 2015 05:38:39 GMT 
 define("kg/swf/0.0.1/index", ["dom"], function(require, exports, module) {
 var dom = require("dom");
 /*
